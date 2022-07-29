2e1f58d61c63f3fdcb13297fda4fba7caf3b1a4d
forward <chen910606@gmail.com>
2022-07-29 18:54:55 +0800

    finished powersupply manipulation

--- experiment/js/exp5.js
+++ experiment/js/exp5.js
@@ -1,16 +1,33 @@
 var VisibleMenu = ''; // 記錄目前顯示的子選單的 ID
 
+const cur1 = document.querySelector("#powersupply1");
+const vol1 = document.querySelector("#powersupply2");
+const cur2 = document.querySelector("#powersupply3");
+const vol2 = document.querySelector("#powersupply4");
+
+const addcurrent1 = document.querySelector("#powersupply5");
+const addvoltage1 = document.querySelector("#powersupply7");
+const addcurrent2 = document.querySelector("#powersupply9");
+const addvoltage2 = document.querySelector("#powersupply11");
+
+const deccurrent1 = document.querySelector("#powersupply6");
+const decvoltage1 = document.querySelector("#powersupply8");
+const deccurrent2 = document.querySelector("#powersupply10");
+const decvoltage2 = document.querySelector("#powersupply12");
+
+var current1 = 0, voltage1 = 0, current2 = 0, voltage2 = 0, power = 0;
+
 // 顯示或隱藏子選單
-function switchMenu( theMainMenu, theSubMenu, theEvent ){
-    var SubMenu = document.getElementById( theSubMenu );
-    if( SubMenu.style.display == 'none' ){ // 顯示子選單
+function switchMenu(theMainMenu, theSubMenu, theEvent) {
+    var SubMenu = document.getElementById(theSubMenu);
+    if (SubMenu.style.display == 'none') { // 顯示子選單
         SubMenu.style.minWidth = theMainMenu.clientWidth; // 讓子選單的最小寬度與主選單相同 (僅為了美觀)
         SubMenu.style.display = 'block';
         hideMenu(); // 隱藏子選單
         VisibleMenu = theSubMenu;
     }
-    else{ // 隱藏子選單
-        if( theEvent != 'MouseOver' || VisibleMenu != theSubMenu ){
+    else { // 隱藏子選單
+        if (theEvent != 'MouseOver' || VisibleMenu != theSubMenu) {
             SubMenu.style.display = 'none';
             VisibleMenu = '';
         }
@@ -18,10 +35,79 @@ function switchMenu( theMainMenu, theSubMenu, theEvent ){
 }
 
 // 隱藏子選單
-function hideMenu(){
-    if( VisibleMenu != '' ){
-        document.getElementById( VisibleMenu ).style.display = 'none';
+function hideMenu() {
+    if (VisibleMenu != '') {
+        document.getElementById(VisibleMenu).style.display = 'none';
     }
     VisibleMenu = '';
 }
 
+document.getElementById("powersupply14").onclick = function () {
+    if (power == 0) {
+        cur1.innerHTML = current1.toFixed(2);
+        cur2.innerHTML = current2.toFixed(2);
+        vol1.innerHTML = voltage1.toFixed(1);
+        vol2.innerHTML = voltage2.toFixed(1);
+        power = 1;
+    } else {
+        current1 = current2 = 0;
+        voltage1 = voltage2 = 0;
+        cur1.innerHTML = "";
+        cur2.innerHTML = "";
+        vol1.innerHTML = "";
+        vol2.innerHTML = "";
+        power = 0;
+    }
+}
+
+addcurrent1.onclick = function () {
+    current1 += 0.01;
+    cur1.innerHTML = current1.toFixed(2);
+}
+
+addvoltage1.onclick = function () {
+    voltage1 += 0.1;
+    vol1.innerHTML = voltage1.toFixed(1);
+}
+
+addcurrent2.onclick = function () {
+    current2 += 0.01;
+    cur2.innerHTML = current2.toFixed(2);
+}
+
+addvoltage2.onclick = function () {
+    voltage2 += 0.1;
+    vol2.innerHTML = voltage2.toFixed(1);
+}
+
+deccurrent1.onclick = function () {
+    current1 -= 0.01;
+    if (current1 < 0) {
+        current1 = 0;
+    }
+    cur1.innerHTML = current1.toFixed(2);
+}
+
+decvoltage1.onclick = function () {
+    voltage1 -= 0.1;
+    if (voltage1 < 0) {
+        voltage1 = 0;
+    }
+    vol1.innerHTML = voltage1.toFixed(1);
+}
+
+deccurrent2.onclick = function () {
+    current2 -= 0.01;
+    if (current2 < 0) {
+        current2 = 0;
+    }
+    cur2.innerHTML = current2.toFixed(2);
+}
+
+decvoltage2.onclick = function () {
+    voltage2 -= 0.1;
+    if (voltage2 < 0) {
+        voltage2 = 0;
+    }
+    vol2.innerHTML = voltage2.toFixed(1);
+}
