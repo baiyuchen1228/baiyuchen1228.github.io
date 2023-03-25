b10e3ef6808d2182fa1d9836d4146d73979af358
forward0606 <chen910606@gmail.com>
2023-03-25 15:34:14 +0800

    update bug in add_horizonal_SWP

--- experiment/js/exp2-2.js
+++ experiment/js/exp2-2.js
@@ -3199,7 +3199,7 @@ function minus_horizonal_SWP(){
 function add_horizonal_SWP(){
     if(osi._SWP > 1.2) return;
     if(osi._SWP > -0.96 && osi._SWP < 1){
-        osi._SWP = 0;
+        osi._SWP = 1;
     }else{
         osi._SWP += 0.04;
     }
