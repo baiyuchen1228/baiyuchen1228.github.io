3cb1d826b9edbf574de46fdf665aa954dd2e14ff
forward0606 <chen910606@gmail.com>
2024-01-26 19:34:44 +0800

    儀器與電路板（或麵包板）上每個點都只能接一條線，只有測驗三與實驗三電源結構的負極例外

--- experiment/js/exp3.js
+++ experiment/js/exp3.js
@@ -516,10 +516,25 @@ $("#container").mouseup(function (e) {
                 return;
             }
         }
-        // if (x1 == x2 && y1 == y2) {
-        //     alert("(不能在同一點畫線)It is meaningless to insert both the ends of wire to the same point.");
-        //     return;
-        // }
+
+        for (let i = 0; i < pointarray.length; i++) {
+            if(pointarray[i][0] == 577 && pointarray[i][0] == 427){
+                // 負極可以接多個
+                continue;
+            }
+            if(pointarray[i][0] == 877 && pointarray[i][0] == 427){
+                // 正極可以接多個
+                continue;
+            }
+            if ((x1 == pointarray[i][0] && y1 == pointarray[i][1]) || (x2 == pointarray[i][0] && y2 == pointarray[i][1])) {
+                alert("(不能在同一點畫線)It is meaningless to insert two wire to the same point.");
+                return;
+            }
+        }
+        if (x1 == x2 && y1 == y2) {
+            alert("(不能在同一點畫線)It is meaningless to insert both the ends of wire to the same point.");
+            return;
+        }
         if (alligatorNo < 10) {
             document.getElementById('svgline2').appendChild(parseSVG('<circle id=alligatorCircle1_0' + alligatorNo + ' cx=' + x1 + ' cy=' + y1 + ' r=' + 5 + ' style="fill:' + colorlist[colorNo] + ';stroke-width:2"><title></title></line>'));
             document.getElementById('svgline2').appendChild(parseSVG('<circle id=alligatorCircle2_0' + alligatorNo + ' cx=' + x2 + ' cy=' + y2 + ' r=' + 5 + ' style="fill:' + colorlist[colorNo] + ';stroke-width:2"><title></title></line>'));
